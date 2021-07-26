877cfdde2530bf64be41cb1ec6e8559b578d1fa3
Developer-felix <onjombafelix1@gmail.com>
2021-07-26 10:59:00 -0700

    Update and delete functionality

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import getRoutes,getNotes,getNote
+from .views import getRoutes,getNotes,getNote,createNote,updateNote,deleteNote
 
 urlpatterns = [
     path('',getRoutes),
     path('notes/',getNotes),
+    path('notes/create/',createNote),
     path('notes/<str:pk>/',getNote),
+    path('notes/<str:pk>/update/',updateNote),
+    path('notes/<str:pk>/delete/',deleteNote),
 ]
